e6d72b2347f0a48313c42c4711c7436e1f509e26
André Yassuhiro Uehara <andreyassuhirouehara@gmail.com>
2014-09-10 10:31:25 -0300

    Create mainwindow.h

---
+++ Uehara/Headers/mainwindow.h
@@ -0,0 +1,22 @@
+#include <regras.h>
+#include <administrador.h>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public slots:
+    void ativarAdministrador();
+public:
+    Administrador TelaAdmin;
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
