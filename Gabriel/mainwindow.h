0182f4fa6fc978c2b26e16be5ad2fa0578c7ac97
gnino <bielnino_scar@hotmail.com>
2014-09-16 21:31:08 -0300

    Create mainwindow.h

---
+++ Gabriel/mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <regras.h>
+#include <administrador.h>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public slots:
+    void ativarAdministrador();
+public:
+    Administrador TelaAdmin;
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
