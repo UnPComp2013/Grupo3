aacd1d74054a6195744b7292476fef988b6051ca
Genival Veloso de França Neto <netoovf@live.com>
2014-09-24 17:33:21 -0300

    Create dados.h

---
+++ Neto/Forms/Headers/dados.h
@@ -0,0 +1,16 @@
+#ifndef DADOS_H
+#define DADOS_H
+#include <QMainWindow>
+namespace Ui {
+class Dados;
+}
+class Dados : public QMainWindow
+{
+Q_OBJECT
+public:
+explicit Dados(QWidget *parent = 0);
+~Dados();
+private:
+Ui::Dados *ui;
+};
+#endif // DADOS_H
