55be311d6eab2bfd463b33d25659593612a74ae3
luhcaf <luhcafi@hotmail.com.br>
2014-09-24 17:46:19 -0300

    Create interfaceLogin.cpp

---
+++ Luana/Sources/interfaceLogin.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+MainWindow::MainWindow(QWidget *parent) :
+QMainWindow(parent),
+ui(new Ui::MainWindow)
+{
+ui->setupUi(this);
+}
+MainWindow::~MainWindow()
+{
+delete ui;
+}
+void MainWindow::on_pushButton_clicked()
+{
+QMessageBox::information(this,tr("Erro!"),tr("Digite Matricula!"));
+}
